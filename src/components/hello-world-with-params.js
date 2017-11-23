6211fa37976cd72d929f7ca36731425061b32d64
Julien Lengrand-Lambert <Julien.Lengrand-Lambert@ing.nl>
2017-11-23 12:05:47 +0100

    Adding component with parameters

---
+++ src/components/hello-world-with-params.js
@@ -0,0 +1,23 @@
+class HelloWorldWithParamsElement extends HTMLElement {
+    static get observedAttributes() {return ['name']; }
+    
+    constructor(){
+        super()
+        
+        this.innerHTML = `<h1>Hello default !</h1>`;
+    }    
+
+    attributeChangedCallback(attributeName, oldValue, newValue, namespace){
+        if(attributeName === "name"){
+            console.log("Attribute name changed!");
+            this.innerHTML = `<h1>Hello ${newValue} !</h1>`;            
+        }
+        else{
+            console.log("Unknown attribute " + attributeName + "changed");
+        }
+    }    
+}
+
+    window.customElements.define(
+                    'hello-world-with-params', 
+                     HelloWorldWithParamsElement);
